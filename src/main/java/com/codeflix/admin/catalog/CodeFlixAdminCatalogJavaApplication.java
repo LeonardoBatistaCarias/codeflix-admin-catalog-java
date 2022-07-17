ed88fb0102c78b90d9562293b318f5f75be12f6a
Leonardo Batista Carias <leonardo1carias@gmail.com>
2022-07-17 18:25:19 -0300

    first commit

---
+++ src/main/java/com/codeflix/admin/catalog/CodeFlixAdminCatalogJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codeflix.admin.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFlixAdminCatalogJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFlixAdminCatalogJavaApplication.class, args);
+	}
+
+}
